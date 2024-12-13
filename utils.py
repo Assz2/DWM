d70fa3f975896c395ff54f726a688accb9a5a8fb
Assz2 <alessandro.sozzo03@gmail.com>
2024-12-13 18:47:47 +0100

    data cleaning v4

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # This function removes the columns from train that are not in test
 def intersect_features(train_set, test_set):
@@ -9,8 +10,8 @@ def intersect_features(train_set, test_set):
     smaller_train = smaller_train.drop(columns=['id'], axis=1)
     return smaller_train
 
-# This function removes the columns from train that have more than a fixed% of NaN (default is 40%)
-def drop_columns(df, threshold = 0.4):
+# This function removes the columns from train that have more than a fixed% of NaN
+def drop_columns(df, threshold):
     minimum_non_NaN = len(df) * threshold   
     dropped_columns = df.columns[df.isnull().sum() > (len(df) - minimum_non_NaN)].tolist()
     new_df = df.drop(columns=dropped_columns)
@@ -35,3 +36,22 @@ def count_feature_clusters(feature_list, feature_clusters):
     #for cluster in feature_clusters:
         #print(f"Cluster {cluster}: {len([col for col in df.columns if cluster in col])}")
     return {cluster: len([col for col in feature_list if cluster in col]) for cluster in feature_clusters}  
+
+# Drop all the records with a fixed % of NaN
+def drop_rows(df, threshold):
+    minimum_non_NaN = len(df.columns) * threshold
+    new_df = df.dropna(thresh=minimum_non_NaN)
+    return new_df
+
+def remove_outliers_iqr(df, droppable_columns):
+    features = df.drop(columns=droppable_columns, axis=1) 
+    
+    for col in features.columns:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 2.5 * IQR
+        upper_bound = Q3 + 2.5 * IQR
+        features = features[(features[col] >= lower_bound) & (features[col] <= upper_bound)]
+    cleaned_df = df.loc[features.index]
+    return cleaned_df
